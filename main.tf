b741d8eddc838f8e10eb2cd9e041cf30a7680c63
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:26 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "protected_settings" {
+  for_each     = { for k, v in var.stack_hci_extensions : k => v if v.protected_settings_key_vault_id != null && v.protected_settings_key_vault_secret_name != null }
+  name         = each.value.protected_settings_key_vault_secret_name
+  key_vault_id = each.value.protected_settings_key_vault_id
+}
 resource "azurerm_stack_hci_extension" "stack_hci_extensions" {
   for_each = var.stack_hci_extensions
 
@@ -7,7 +12,7 @@ resource "azurerm_stack_hci_extension" "stack_hci_extensions" {
   type                               = each.value.type
   auto_upgrade_minor_version_enabled = each.value.auto_upgrade_minor_version_enabled
   automatic_upgrade_enabled          = each.value.automatic_upgrade_enabled
-  protected_settings                 = each.value.protected_settings
+  protected_settings                 = each.value.protected_settings != null ? each.value.protected_settings : try(data.azurerm_key_vault_secret.protected_settings[each.key].value, null)
   settings                           = each.value.settings
   type_handler_version               = each.value.type_handler_version
 }
--- variables.tf
+++ variables.tf
@@ -10,20 +10,24 @@ Optional:
     - auto_upgrade_minor_version_enabled
     - automatic_upgrade_enabled
     - protected_settings
+    - protected_settings_key_vault_id (alternative to protected_settings - read from Key Vault instead)
+    - protected_settings_key_vault_secret_name (alternative to protected_settings - read from Key Vault instead)
     - settings
     - type_handler_version
 EOT
 
   type = map(object({
-    arc_setting_id                     = string
-    name                               = string
-    publisher                          = string
-    type                               = string
-    auto_upgrade_minor_version_enabled = optional(bool) # Default: true
-    automatic_upgrade_enabled          = optional(bool) # Default: true
-    protected_settings                 = optional(string)
-    settings                           = optional(string)
-    type_handler_version               = optional(string)
+    arc_setting_id                           = string
+    name                                     = string
+    publisher                                = string
+    type                                     = string
+    auto_upgrade_minor_version_enabled       = optional(bool) # Default: true
+    automatic_upgrade_enabled                = optional(bool) # Default: true
+    protected_settings                       = optional(string)
+    protected_settings_key_vault_id          = optional(string)
+    protected_settings_key_vault_secret_name = optional(string)
+    settings                                 = optional(string)
+    type_handler_version                     = optional(string)
   }))
 }
 
